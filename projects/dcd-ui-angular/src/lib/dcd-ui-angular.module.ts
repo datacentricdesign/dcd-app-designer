b6ffe6bd242a0fa861d865b321dc91cb0143692a
issarompion <irompion@yahoo.fr>
2019-08-02 11:34:05 +0200

    orga tsconfig & update lib

--- projects/dcd-ui-angular/src/lib/dcd-ui-angular.module.ts
+++ projects/dcd-ui-angular/src/lib/dcd-ui-angular.module.ts
@@ -1,34 +1,50 @@
-import { NgModule } from '@angular/core';
-import { DcdUiAngularComponent } from './dcd-ui-angular.component';
-// import { DoubleAxisChartComponent } from './double-axis-chart/double-axis-chart.component';
-import { LineChartComponent } from './line-chart/line-chart.component';
-import { RadarChartComponent } from './radar-chart/radar-chart.component';
-
-
-//PrimeNG
-import {DialogModule} from 'primeng/dialog';
+import {NgModule} from '@angular/core';
+/*import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CalendarModule} from 'primeng/calendar';
-import {SliderModule} from 'primeng/slider';
-import {CheckboxModule} from 'primeng/checkbox';
-import {InputTextModule} from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
 
 // Ng2 charts
 import { ChartsModule } from 'ng2-charts';
 
-// ngx charts
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//PrimeNG
+import {DialogModule} from 'primeng/dialog';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {CheckboxModule} from 'primeng/checkbox';
+import {InputTextModule} from 'primeng/inputtext';*/
+
+import { DcdUiAngularComponent } from './dcd-ui-angular.component';
+//import { DoubleAxisChartComponent } from './double-axis-chart/double-axis-chart.component';
+//import { LineChartComponent } from './line-chart/line-chart.component';
+//import { RadarChartComponent } from './radar-chart/radar-chart.component';
+//import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
+
+
+
 
 @NgModule({
-  declarations: [DcdUiAngularComponent, /*DoubleAxisChartComponent,*/ LineChartComponent, RadarChartComponent],
-  imports: [
-    NgxChartsModule,
-    NgbModule,
-    ChartsModule,
-    CalendarModule,
-    SliderModule
+  declarations: [
+    DcdUiAngularComponent,
+    //RadarChartComponent,
+    //NavbarComponent,
+    UserComponent
   ],
-  exports: [DcdUiAngularComponent]
+  imports: [
+    //FormsModule,
+    //ChartsModule,
+    //SliderModule,
+    //CommonModule
+    //BrowserAnimationsModule,
+    //BrowserModule
+  ],
+  exports: [
+    DcdUiAngularComponent,
+    //RadarChartComponent,
+    //NavbarComponent,
+    UserComponent,
+    
+  ]
 })
 export class DcdUiAngularModule { }
